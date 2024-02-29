f2a934842a2398781a69fa1f88a907b69af7733c
Marlon-Trujillo-Jaramillo <118902850+Marlon-Trujillo-Jaramillo@users.noreply.github.com>
2024-02-29 13:43:49 -0500

    Actualizacion

---
+++ Calculadora/Asignatura.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    internal class Asignatura
+    {
+        public string Nombre { get; private set; }
+        public List<(double calificacion, double porcentaje)> Calificaciones { get; private set; }
+
+        public Asignatura()
+        {
+            Calificaciones = new List<(double, double)>();
+        }
+
+        public void AgregarCalificacion(double calificacion, double porcentaje)
+        {
+            Calificaciones.Add((calificacion, porcentaje));
+            Console.WriteLine("Calificación agregada correctamente.");
+        }
+
+        public double CalcularPromedioAcumulado()
+        {
+            double sumaProductos = 0;
+            double sumaPorcentajes = 0;
+
+            foreach (var calificacion in Calificaciones)
+            {
+                sumaProductos += calificacion.calificacion * calificacion.porcentaje;
+                sumaPorcentajes += calificacion.porcentaje;
+            }
+
+            if (sumaPorcentajes == 0)
+            {
+                Console.WriteLine("No se pueden calcular promedio, no hay notas ingresadas.");
+                return 0;
+            }
+
+            return sumaProductos / sumaPorcentajes;
+        }
+
+        public double CalcularPromedioMinimo(double notaDeseada)
+        {
+            double sumaPorcentajes = 0;
+            foreach (var calificacion in Calificaciones)
+            {
+                sumaPorcentajes += calificacion.porcentaje;
+            }
+
+            double porcentajeRestante = 100 - sumaPorcentajes;
+            if (porcentajeRestante <= 0)
+            {
+                Console.WriteLine("No se puede calcular el promedio mínimo, se han ingresado el 100% de las notas.");
+                return 0;
+            }
+
+            double sumaProductos = notaDeseada * porcentajeRestante;
+            foreach (var calificacion in Calificaciones)
+            {
+                sumaProductos -= calificacion.calificacion * calificacion.porcentaje;
+            }
+
+            return sumaProductos / porcentajeRestante;
+        }
+
+        public void IngresarDatosAsignatura()
+        {
+            Console.Write("Ingrese el nombre de la asignatura: ");
+            Nombre = Console.ReadLine();
+
+            Console.Write("Ingrese la cantidad de notas: ");
+            int cantidadNotas = Convert.ToInt32(Console.ReadLine());
+
+            double porcentajeTotal = 0;
+            for (int i = 0; i < cantidadNotas; i++)
+            {
+                Console.Write($"Ingrese el porcentaje de la nota {i + 1}: ");
+                double porcentaje = Convert.ToDouble(Console.ReadLine());
+
+                while (porcentajeTotal + porcentaje > 100)
+                {
+                    Console.WriteLine("Error: La suma de los porcentajes no puede superar el 100%.");
+                    Console.Write($"Ingrese un porcentaje válido para la nota {i + 1}: ");
+                    porcentaje = Convert.ToDouble(Console.ReadLine());
+                }
+                porcentajeTotal += porcentaje;
+
+                Console.Write($"Ingrese la calificación de la nota {i + 1}: ");
+                double calificacion = Convert.ToDouble(Console.ReadLine());
+
+                AgregarCalificacion(calificacion, porcentaje);
+            }
+        }
+    }
+}
--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -10,6 +10,26 @@ namespace Calculadora
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Asignatura matematicas = new Asignatura();
+                matematicas.IngresarDatosAsignatura();
+
+                // Calcular promedio acumulado de las calificaciones
+                double promedioAcumulado = matematicas.CalcularPromedioAcumulado();
+                Console.WriteLine($"El promedio acumulado de {matematicas.Nombre} es: {promedioAcumulado}");
+
+                // Calcular promedio mínimo para alcanzar una nota deseada
+                Console.Write("Ingrese la nota deseada: ");
+                double notaDeseada = Convert.ToDouble(Console.ReadLine());
+                double promedioMinimo = matematicas.CalcularPromedioMinimo(notaDeseada);
+                Console.WriteLine($"El promedio mínimo necesario para obtener una nota de {notaDeseada} en {matematicas.Nombre} es: {promedioMinimo}");              
+            }
+            catch 
+            { 
+                Console.WriteLine("Verifique su informacion"); 
+            }
+            
         }
     }
 }
